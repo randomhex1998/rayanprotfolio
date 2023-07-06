0ffc256551765b471e0cde2b81e05dc0201317dd
randomhex1998 <randomhex1998@gmail.com>
2023-07-06 16:25:18 +0330

    make routes and home page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+//tools
+import {Route , Routes } from "react-router-dom"
+
+
+//Components
+import Home from "./Pages/Home"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route index path="/" element={<Home/>}/>
+      </Routes>
+    </>
+      
+  )
+}
+
+export default App
