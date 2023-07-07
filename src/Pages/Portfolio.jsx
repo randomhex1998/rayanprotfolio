3b6da4ac8ae6f15a73006e2462888eede1c1d6d8
randomhex1998 <randomhex1998@gmail.com>
2023-07-07 09:52:31 +0330

    make sreach box and its style

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React , {useState} from "react";
 import Navbar from "../components/Navbar/Navbar";
 import pr from "../assets/pr.jpeg";
 function Portfolio() {
+  const [inp , setInp ] = useState("");
+  console.log(inp)
   return (
     <div>
       <Navbar />
@@ -9,6 +11,15 @@ function Portfolio() {
         className="w-[100%] h-[80vh]"
         style={{ backgroundImage: `url(${pr})`, backgroundSize: "cover" }}
       ></div>
+      <div className="container mx-auto">
+          <input
+            type="text"
+            className="px-2 border border-gray-300 outline-none w-full h-[40px] rounded-lg my-8"
+            placeholder="Search in Projects"
+            onChange={e => setInp(e.target.value)}
+            value={inp}
+            />
+      </div>
     </div>
   );
 }
